96728fd6b4363838c595a84c16f9b22a5cef0a46
Loki hornsby <lokihornsby@gmail.com>
2022-02-23 23:02:11 +0000

    Updated to v2.0!
    woop woop! :smiley:

--- Info.py
+++ Info.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMessageBox, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui
 
 import os
 import platform
@@ -10,51 +11,88 @@ from datetime import datetime
 import os
 
 class Window(QWidget):
-
     # Initialize app
     def __init__(self):
         super(Window, self).__init__()
 
-        # Folder location
-        self.Folder = ""
-
-        # Counter
-        self.created = 0
-        self.ignored = 0
-        
-        # Get folder location
-        self.request_location()
+        # Stay ontop
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
         # Set Filename
         self.fname = "Info.txt"
 
-        # fill folders with text file
-        self.fill_folders()
+        # Main Func
+        self.Main()
 
-        # Show finish dialogue
-        self.ShowFinishDialogue()
+    # Main Function
+    def Main(self):
+        # Folder location
+        self.FolderLocation = ""
 
-    def void(self):
-        print("Done!")
+        # Get folder location and exit if cancel
+        if self.request_location():
+            # Setup path and folders
+            self.path = self.FolderLocation + r"/"
+            self.folders = next(os.walk(self.path))[1]
 
-    # Finished popup
-    def ShowFinishDialogue(self):
-        self.msgBox = QMessageBox(self)
-        self.msgBox.setIcon(QMessageBox.Information)
-        self.msgBox.setText(str(self.created) + " files created!" + "\n" + str(self.ignored) + " files alreay exist!")
-        self.msgBox.setWindowTitle("Info!")
-        self.msgBox.setStandardButtons(QMessageBox.Ok)
-        self.msgBox.buttonClicked.connect(self.void)
-        self.msgBox.show() # this is so stupid but it makes sense why this exists - took me a good 20 minutes to add this line of code in :P
+            # Setup Amount
+            self.created = 0
+            self.ignored = 0
+            self.get_amount()
+            
+            # Confirm
+            if self.Confirm():
+                # Fill folders with text files
+                self.fill_folders()
+
+                # Show finish dialogue
+                self.ShowDialogue( 
+                    QMessageBox.Information,
+                    str(self.created) + " files created!" + "\n" + str(self.ignored) + " files alreay exist!",
+                    "Info!",
+                    lambda:quit(),
+                    QMessageBox.Ok,
+                )
+
+    # Dynamic Popup
+    def ShowDialogue(self, icon, text, title, action, buttons):
+        msgBox = None
+
+        msgBox = QMessageBox(self)
+        msgBox.setIcon(icon)
+        msgBox.setText(text)
+        msgBox.setWindowTitle(title)
+        msgBox.buttonClicked.connect(action)
+        msgBox.setStandardButtons(buttons)
+
+        msgBox.show()
+
+        return msgBox
 
     # Request folder location
     def request_location(self):
-        self.Folder = QFileDialog.getExistingDirectory(self, "Select Directory")
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.Directory)
+        dialog.setViewMode(QFileDialog.Detail)
+
+        if dialog.exec():
+            self.FolderLocation = dialog.directory().absolutePath()
+
+            return True
+        else:
+            self.ShowDialogue( 
+                QMessageBox.Information,
+                "Cancelled Operation",
+                "Info!",
+                lambda:quit(),
+                QMessageBox.Ok,
+            )
+
+            return False
 
     # Get creation date of folder
     def creation_date(self, path_to_file):
         if platform.system() == 'Windows':
-            # this is stupid :P
             literal_time = os.path.getctime(path_to_file)
             converted_time = str(datetime.fromtimestamp(literal_time).strftime('This folder was created on %Y-%m-%d at %H:%M:%S!'))
 
@@ -62,30 +100,51 @@ class Window(QWidget):
         else:
             print("Not supported!")
 
-    # Write info to folder
-    def fill_folders(self):
-        path = self.Folder + r"/"
-        folders = next(os.walk(path))[1]
-        print(os.listdir())
+    # Get Folder Amount
+    def get_amount(self):
+        for folder in self.folders:
+            local_path = self.path + folder + r"/" + self.fname
 
-        for folder in folders:
-            local_path = path + folder + r"/" + self.fname
-            print(local_path)
-                
             if not os.path.exists(local_path):
-                f = open(local_path, "w+")
-                f.write(self.creation_date(local_path))
-                f.close()
-
                 self.created += 1
             else:
                 self.ignored += 1
 
-        #print(str(created) + " files created")
-        #print(str(ignored) + " files already created")
-        
-        #quit()
-        
+    # Confirm Writing
+    def Confirm(self):
+        confirm = self.ShowDialogue( 
+            QMessageBox.Warning,
+            "Warning! \n" + "This will create " + str(self.created) + " files\nare you sure?",
+            "Warning!",
+            lambda:print("User Selected: "), 
+            QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel
+        )
+
+        ex = confirm.exec()
+
+        if ex == QMessageBox.Yes:
+            print("Yes....Writing to location....")
+            return True
+        elif ex == QMessageBox.No:
+            print("No....Reopening Folder Select....")
+            self.Main()
+            return False
+        elif ex == QMessageBox.Cancel:
+            print("Cancel....Quitting Application....")
+            quit()
+            return False
+            
+    # Write info to folder
+    def fill_folders(self):
+        for folder in self.folders:
+            local_path = self.path + folder + r"/" + self.fname
+            if not os.path.exists(local_path):
+                f = open(local_path, "w+")
+                f.write(self.creation_date(local_path))
+                f.close()
+                print("Wrote to: " + str(local_path))
+
+
 # Run App
 if __name__ == '__main__':
     # create pyqt5 app
@@ -93,9 +152,6 @@ if __name__ == '__main__':
   
     # create the instance of our Window
     window = Window()
-
-    # Show window
-    #window.show()
 
     sys.exit(App.exec_())
 
